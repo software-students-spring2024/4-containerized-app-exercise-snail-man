71d53ef01e961673dd664bfa7997db6a9a67e0db
Josckar <jp5936@nyu.edu>
2024-04-09 10:50:06 -0400

    add script for building docker web-app, fix liintiing errors, and modify Do ckerfile

--- web-app/app.py
+++ web-app/app.py
@@ -1,7 +1,13 @@
-from flask import Flask, jsonify, render_template, request
+"""
+
+Web-App for displayinig the data from ../machine-learning client in 
+a (hopefully) Human-Readable Format
+
+"""
+import os
+from flask import Flask, render_template
 import requests
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
 
@@ -11,11 +17,17 @@ ML_CLIENT_URL = os.getenv('ML_CLIENT_URL', 'http://localhost:5001/receive_data')
 
 @app.route('/display-data', methods=['GET'])
 def request_data_and_display_result():
+    """
+    Gets JSON of data from API Endpont ML_CLENT_URL and renders it in result.html
 
+    Returns:
+        html: Table displaying data
+
+    """
     data_to_send = {"data": "Your data to be analyzed"}
-    
-    response = requests.post(ML_CLIENT_URL, json=data_to_send)
-    
+
+    response = requests.post(ML_CLIENT_URL, json=data_to_send, timeout=10)
+
     analysis_result = response.json()
 
     return render_template('result.html', analysis_result=analysis_result)
