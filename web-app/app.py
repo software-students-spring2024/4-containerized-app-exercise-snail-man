031306dde846eb2850fa90e0fd254c819635b167
jsh9965 <jsh9965@nyu.edu>
2024-04-09 13:22:40 -0400

    formatted

--- web-app/app.py
+++ web-app/app.py
@@ -4,6 +4,7 @@ Web-App for displayinig the data from ../machine-learning client in
 a (hopefully) Human-Readable Format
 
 """
+
 import os
 from flask import Flask, render_template, request
 import requests
@@ -14,25 +15,26 @@ load_dotenv()
 
 app = Flask(__name__)
 
-ML_CLIENT_URL = os.getenv('ML_CLIENT_URL', 'http://localhost:5001/receive_data')
+ML_CLIENT_URL = os.getenv("ML_CLIENT_URL", "http://localhost:5001/receive_data")
 
-@app.route('/add-face', methods=['GET', 'POST'])
+
+@app.route("/add-face", methods=["GET", "POST"])
 def add_face():
-    if request.method == 'POST':
+    if request.method == "POST":
         # Decode base64 image data
-        image_data = request.form['image_data']
+        image_data = request.form["image_data"]
         image_data = base64.b64decode(image_data.split(",")[1])
 
         # Write image to file
-        with open('images/captured_image.jpg', 'wb') as f:
+        with open("images/captured_image.jpg", "wb") as f:
             f.write(image_data)
-        
+
         return "Image captured successfully!"
 
-    return render_template('addFace.html')
+    return render_template("addFace.html")
 
 
-@app.route('/display-data', methods=['GET'])
+@app.route("/display-data", methods=["GET"])
 def request_data_and_display_result():
     """
     Gets JSON of data from API Endpont ML_CLENT_URL and renders it in result.html
@@ -47,7 +49,8 @@ def request_data_and_display_result():
 
     analysis_result = response.json()
 
-    return render_template('result.html', analysis_result=analysis_result)
+    return render_template("result.html", analysis_result=analysis_result)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
